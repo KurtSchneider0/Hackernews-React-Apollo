ef4d52d26cf741d8ddeb54dd0131490285ccca47
kurtschneider0 <sp4rky0ne@protonmail.com>
2022-01-15 15:21:08 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Search from './Search';
+
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Routes>
+        <Route exact path="/" element={<LinkList/>}
+        />
+        <Route
+          exact
+          path="/create"
+          element={<CreateLink/>}
+        />
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/search" element={<Search/>} />
+      </Routes>
+    </div>
+  </div>
+);
+
+export default App;
